f8c3109dcecf2dd7013b357a29c8e8b0e5c89a2a
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-05-24 21:36:33 +0700

    feat : work on controller.js

--- controller/appController.js
+++ controller/appController.js
@@ -1,3 +1,7 @@
+import UserModel from '../model/user.js';
+import bcrypt from 'bcrypt';
+
+
 /** POST http://localhost:8080/api/register
  * @param : {
     "username" : "example123",
@@ -12,7 +16,36 @@
 }
  */
  exports.register = async (req, res) => {
-    res.json({"message" : "register"})
+    try{
+      const {username, password, profile, email} = req.body;
+
+      const existUsername = new Promise((resolve, reject) => {
+        UserModel.findOne({username}, (err, user) => {
+          if(err) reject(new Error(err));
+          if(user) reject({error: 'Username exist, please use unique username'})
+
+          resolve();
+        })
+      });
+
+      const existEmail = new Promise((resolve, reject) => {
+        UserModel.findOne({email}, (err, user) => {
+          if(err) reject(new Error(err));
+          if(user) reject({error: 'Email exist, please use unique email'})
+
+          resolve();
+        })
+      })
+
+      Promise.all([existUsername, existEmail]).then(() => {
+        
+      }).catch((err) => {
+        return res.status(500).send({error : 'Enable to hashed password'})
+      })
+
+    } catch(e){
+      res.status(500).send()
+    }
  }
 
  /** POST http://localhost:8080/api/login
---
+++ model/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export const UserSchema = mongoose.Schema({
+    usernames: {
+        type: String,
+        required: [true, 'Please provide uniqe username'],
+        unique: [true, 'Username exist']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide password'],
+        unique: false,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        unique: [true, 'Email exist']
+    },
+    firstName: {type: String},
+    lastName: {type: String},
+    phone: {type: Number},
+    address: {type: String},
+    profile: {type: String}
+});
+
+export default mongoose.model.Users || mongoose.model('User', UserSchema)
